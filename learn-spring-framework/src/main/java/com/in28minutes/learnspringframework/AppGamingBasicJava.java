a65347cce32740d4cffb8f976c4143058b6db260
pipi-shortstocking <jannie435@naver.com>
2024-04-17 16:00:54 +0900

    느슨한 결합과 강합 결합 알아보기

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,11 +2,16 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var marioGame = new MarioGame(); // Mario 게임 인스턴스 생성
-        var gameRunner = new GameRunner(marioGame); // GameRunner로 Mario 게임 실행
+//        var marioGame = new MarioGame(); // Mario 게임 인스턴스 생성
+//        var gameRunner = new GameRunner(marioGame); // GameRunner로 Mario 게임 실행
+
+        // GameRunner 클래스가 특정 게임과 강하게 결합하고 있음을 알 수 있음
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);
         gameRunner.run();
     }
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,11 +1,17 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-    MarioGame game;
+//    MarioGame game; // Mario 게임과 강한 결합을 하고 있음
 
-    public GameRunner(MarioGame game) {
+//    public GameRunner(MarioGame game) {
+//        this.game = game;
+//    }
+    private SuperContraGame game;
+
+    public GameRunner(SuperContraGame game) {
         this.game = game;
     }
+
 
     public void run() {
         System.out.println("Running game: " + game);
