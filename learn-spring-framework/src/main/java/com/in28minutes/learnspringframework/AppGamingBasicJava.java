0b59c398deeefcca807794b1237e259bb6cb9d7d
pipi-shortstocking <jannie435@naver.com>
2024-04-19 01:21:54 +0900

    Java 인터페이스를 도입하여 느슨하게 결합된 앱 만들기

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -1,8 +1,7 @@
 package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.MarioGame;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
@@ -10,8 +9,14 @@ public class AppGamingBasicJava {
 //        var gameRunner = new GameRunner(marioGame); // GameRunner로 Mario 게임 실행
 
         // GameRunner 클래스가 특정 게임과 강하게 결합하고 있음을 알 수 있음
-        var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(superContraGame);
+//        var superContraGame = new SuperContraGame();
+//        var gameRunner = new GameRunner(superContraGame);
+//        gameRunner.run();
+
+//        var game = new MarioGame();
+//        var game = new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole{
+    public void up() {
+        System.out.println("go above");
+    }
+
+    public void down() {
+        System.out.println("look down");
+    }
+
+    public void left() {
+        System.out.println("go left");
+    }
+
+    public void right(){
+        System.out.println("go right");
+    }
+}
