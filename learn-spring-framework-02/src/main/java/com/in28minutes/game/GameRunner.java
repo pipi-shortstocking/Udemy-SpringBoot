fdc59e5c4b8e12d6ee29aaeb0e995b0b3fd81338
pipi-shortstocking <jannie435@naver.com>
2024-06-19 00:56:13 +0900

    Java 객체의 생성 및 관리를 위한 Spring Framework 이해하기 진행 중

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/game/GameRunner.java
@@ -0,0 +1,29 @@
+package com.in28minutes.game;
+
+public class GameRunner {
+//    MarioGame game; // Mario 게임과 강한 결합을 하고 있음
+
+//    public GameRunner(MarioGame game) {
+//        this.game = game;
+//    }
+//    private SuperContraGame game;
+//
+//    public GameRunner(SuperContraGame game) {
+//        this.game = game;
+//    }
+
+    // 게임이 클래스를 실행하고, 클래스에서 직접 실행하는 대신 인터페이스를 사용하도록 함
+    // 게임을 바꾸려 할 때 GameRunner 클래스에는 아무 변경도 가할 필요가 없음 - 특정 인터페이스와만 결합되어 있기 때문
+    private GamingConsole game;
+
+    public GameRunner(GamingConsole game) {
+        this.game = game;
+    }
+    public void run() {
+        System.out.println("Running game: " + game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/game/SuperContraGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.game;
+
+public class SuperContraGame implements GamingConsole { // GamingConsole 도입
+    public void up() {
+        System.out.println("up");
+    }
+
+    public void down() {
+        System.out.println("Sit down");
+    }
+
+    public void left() {
+        System.out.println("Go back");
+    }
+
+    public void right() {
+        System.out.println("Shoot a bullet");
+    }
+}
