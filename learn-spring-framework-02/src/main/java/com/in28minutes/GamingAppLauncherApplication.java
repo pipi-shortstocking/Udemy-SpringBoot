3dcef04d84396904ed0cff9ccb520c1c87c0aedd
pipi-shortstocking <jannie435@naver.com>
2024-06-26 16:22:38 +0900

    Java 객체의 생성 및 관리를 위한 Spring Framework 이해하기

--- learn-spring-framework-02/src/main/java/com/in28minutes/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/GamingAppLauncherApplication.java
@@ -3,31 +3,23 @@ package com.in28minutes;
 import com.in28minutes.game.GameRunner;
 import com.in28minutes.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.in28minutes.game") // 특정 패키지에서 PacmanGame을 검색해야 한다고 알려줘야 함
-public class App03GamingSpringBeans {
-    @Bean
-    public GameRunner gameRunner(GamingConsole game) { // PacmanGame을 GamingConsole로 Autowiring 가능
-        System.out.println("Parameter: " + game);
-        // Spring이 PacmanGame의 인스턴스를 생성하고, 이 인스터스가 Autowiring되어 PacmanGame이 매개변수 임을 확인 가능
-        var gameRunner = new GameRunner(game);
-        return gameRunner;
-    }
+public class GamingAppLauncherApplication {
 
     public static void main(String[] args) {
 
         try (var context =
                      new AnnotationConfigApplicationContext
-                             (App03GamingSpringBeans.class)) {
+                             (GamingAppLauncherApplication.class)) {
 
             context.getBean(GamingConsole.class).up();
 
             context.getBean(GameRunner.class).run();
-
+            // 수동으로 PacmanGame과 GameRunner를 직접 생성하지 않아도 Spring 프레임워크가 자동으로 대신 생성 가능
         }
     }
 }
